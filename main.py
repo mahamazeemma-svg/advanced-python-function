1571415339a63f448fa8f3090378ef4ba1dba5dc
mahamazeemma-svg <mahamazeem.ma@gmail.com>
2026-09-19 14:05:51 +0100

    maps

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("my name is aaban")
+number1 = [1,2,3]
+number2 = [4,5,6]
+result = map(lambda x, y: x + y, number1,number2)
+print(list(result))
+
+num = [1,2,3,4,5]
+def sq(n):
+    return n*n
+square = list(map(sq,num))
+print(square)
